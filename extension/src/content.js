606d9209257d83aecbd6946651e847d204f5df9d
CuboidRaptor <fanjas112358@gmail.com>
2022-04-28 08:44:36 -0400

    new debugging
    ALSO YOUTUBE WHYYYYYY
    WHY IS YOUR CODING SO DUMB?

--- extension/src/content.js
+++ extension/src/content.js
@@ -11,13 +11,13 @@ function process(blocklist) {
 	// Get current tab URL
 	var currentURL = location.href;
 	
-	console.log("debug, NoRick has been ran")
-	console.log("debug, supposed current URL is " + currentURL)
+	console.log("debug: NoRick has been ran")
+	console.log("debug: supposed current URL is " + currentURL)
 	
 	//Processing, c'mon youtube couldn't you have used reasonable code
 	if (currentURL.endsWith("&feature=youtu.be")) {
 		currentURL = currentURL.slice(0, -("&feature=youtu.be".length));
-		console.log("debug, youtu.be detected, end processed URL is " + currentURL)
+		console.log("debug: youtu.be detected, end processed URL is " + currentURL)
 	}
 	
 	for (let i = 0; i < blocklist.length; i++) {
@@ -25,7 +25,7 @@ function process(blocklist) {
 		if (blocklist[i] == currentURL) {
 			// Is a rickroll, kill the site
 			window.stop();
-			console.log("rickroll");
+			console.log("debug: that's a rickroll");
 			alert("ok so um that's a rickroll");
 			break;
 		}
